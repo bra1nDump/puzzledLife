9fbced75cd0e4d09358d67bc6691b725f90564cd
Kirill Dubovitskiy <kirill2003de@gmail.com>
2018-06-09 02:04:28 -0700

    adding some `link mining` capabilities whithin the Puzzle module. Not working so far lol

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import System.Random
+
 import Lib
 import Piece
+import Puzzle
 import Prelude as P
 import Graphics.Image
+import Graphics.Image.Interface
 import Graphics.Image.IO
 
 -- displayImageUsing
@@ -19,19 +23,64 @@ import Graphics.Image.IO
 projectRoot = "/Users/kirill/root/programming/projects/puzzledLife/"
 viewer = ExternalViewer "open" [] 0
 
+display :: Piece -> IO ()
+display image = do
+  displayImageUsing viewer True image
+
+cluster :: IO Piece
+cluster = readImageRGB VU $ projectRoot ++ "static/cluster.jpg"
+
+centaurus :: IO Piece
+centaurus = readImageRGB VU $ projectRoot ++ "static/centaurus.jpg"
+
+frog :: IO Piece
+frog = readImageRGB VU $ projectRoot ++ "static/frog.jpg"
+
+sampleMask = [(10,10,200,100), (12,5,150,200), (40,40,70,70)]
+
 testHIP1 :: IO ()
 testHIP1 = do
-  cluster <- readImageRGB VU $ projectRoot ++ "static/cluster.jpg"
-  centaurus <- readImageRGB VU $ projectRoot ++ "static/centaurus.jpg"
+  cluster <- cluster
+  centaurus <- centaurus
   let mergedImage = (cluster + centaurus) / 2
-  displayImageUsing viewer True mergedImage
+  display mergedImage
 
 testPieceMask1 :: IO ()
 testPieceMask1 = do
   cluster <- readImageRGB VU $ projectRoot ++ "static/cluster.jpg"
-  let mask = fromList (dims cluster) [(10,10,200,100), (12,5,150,200), (40,40,70,70)]
-  displayImageUsing viewer True $ applyMask mask cluster
+  let mask = fromList (dims cluster) sampleMask
+  display $ applyMask mask cluster
+
+testPieceHash1 :: IO ()
+testPieceHash1 = do
+  cluster <- cluster
+  centaurus <- centaurus
+  let clusterID = hash cluster
+      centaurusID = hash centaurus
+      clusterSubPiece = applyMasks sampleMask cluster
+      clusterSubPieceID = hash clusterSubPiece
+      clusterSubPieceID' = hashWithMask sampleMask cluster
+      -- guard clusterSubPieceID = clusterSubPieceID'
+  putStrLn "clusterID centaurusID"
+  print clusterID
+  print centaurusID
+  putStrLn "subPiece (cluster)"
+  print clusterSubPieceID
+  print clusterSubPieceID'
+  display clusterSubPiece
+
+testPuzzleMake1 :: IO ()
+testPuzzleMake1 = do
+  cluster <- cluster
+  centaurus <- centaurus
+  frog <- frog
+  gen <- getStdGen
+  let clusterID = hash cluster
+      frogID = hash frog
+      puzzle = makePuzzle "Bra1nDump" [cluster, centaurus]
+      niceMask = mineLink 3 gen 1000 clusterID frogID puzzle
+  print niceMask
 
 main :: IO ()
 main = do
-  testPieceMask1
+  testPuzzleMake1
